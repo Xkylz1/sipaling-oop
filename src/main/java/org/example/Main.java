bce4922facb905a23206e6381c64be4ea1b6e1ba
Xkylz1 <fauzansaputra741@gmail.com>
2022-11-22 21:55:19 +0800

    pertama kali

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+Orang iril = new Orang("Iril",20,200);
+        System.out.println(iril.nama1);
     }
 }
---
+++ src/main/java/org/example/Orang.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Orang {
+    String nama1;
+    int umur1;
+    int tinggi1;
+
+    public Orang(String nama, int umur, int tinggi) {
+        nama1 = nama;
+        umur1 = umur;
+        tinggi1 = tinggi;
+    }
+}
